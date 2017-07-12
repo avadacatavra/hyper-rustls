a81f6dae123cdd5a97695e300fc4c282b2a5ec35
ddh <dianehosfelt@gmail.com>
2017-07-12 21:05:31 +0100

    added timing benchmarks

---
+++ examples/bench.rs
@@ -0,0 +1,68 @@
+extern crate futures;
+extern crate hyper;
+extern crate hyper_rustls;
+extern crate rustls;
+extern crate tokio_core;
+
+use std::fs::File;
+use std::path::Path;
+use std::io::Read;
+use std::time::{Duration, Instant};
+
+use hyper::{Client, Uri};
+use std::str::FromStr;
+
+use hyper_rustls::HttpsConnector;
+use hyper::StatusCode;
+
+use tokio_core::reactor::Core;
+
+fn duration_nanos(d: Duration) -> f64 {
+    (d.as_secs() as f64) + (d.subsec_nanos() as f64) / 1e9
+}
+
+// uses default configuration
+fn create_client(core: &Core) -> Client<HttpsConnector> {
+	Client::configure()
+         .connector(HttpsConnector::new(4, &core.handle()))
+         .build(&core.handle())
+}
+
+pub fn website_bench() {
+	let mut file = match File::open(Path::new("./examples/sites.txt")) {
+        Err(_) => return,   //fail silently TODO probably shouldn't...
+        Ok(file) => file,
+    };
+
+   
+    // can create a custom root-ca store (defaults to webpki)
+
+    let mut sites = String::new();
+    file.read_to_string(&mut sites).unwrap();
+
+    let start = Instant::now();
+    let mut core = tokio_core::reactor::Core::new().unwrap();
+    let client = create_client(&core);
+    println!("Client creation time: {}", duration_nanos(Instant::now().duration_since(start)));
+
+    let mut times = vec!();
+    for line in sites.lines(){
+        let l: Vec<String> = line.split(',').map(|s| s.to_string()).collect();
+        let mut site = "https://".to_owned();
+        site.push_str(l[0].trim());
+        //let expected = l[1].trim();
+
+        let start = Instant::now();
+        let res = core.run(client.get(Uri::from_str(&site).unwrap())).unwrap();
+        assert!(res.status().is_success() || res.status() == StatusCode::Found);
+
+        times.push(duration_nanos(Instant::now().duration_since(start)));
+    }
+    println!("{:?}", times);
+
+}
+
+
+fn main() {
+	website_bench();
+}
